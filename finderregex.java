7251cbcc83e2f11276bc78caade7a1be3b0ce4c5
ashpritx <110691342+ashpritx@users.noreply.github.com>
2022-11-01 17:29:19 +0000

    Update finderregex.java

--- finderregex.java
+++ finderregex.java
@@ -1,19 +1,18 @@
-
 import java.util.*;
 
 
-public class finderregex {
+public class fff {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NullPointerException{
 		try {
-			HashMap<String, Integer> score = new HashMap<String,Integer>();
-			score.put("Fail",39);
-			score.put("Third Class", 49);
-			score.put("Second Class, Division 2",59);
-			score.put("Second Class, Division 1",69);
-			score.put("First Class",70);
+			LinkedHashMap<Integer, String> score = new LinkedHashMap<Integer,String>();
+			score.put(70, "First class");
+			score.put(59, "Second Class, Division 1");
+			score.put(49, "Second Class, Divison 2");
+			score.put(39, "Third Class");
 			
 			Scanner scan = new Scanner(System.in);
+			
 			
 			System.out.println("What was your mark?");
 			
@@ -22,12 +21,20 @@ public class finderregex {
 			if(mark >100 && mark < 0) {
 				System.out.println("Invalid Input: out of range.");
 			}
-			for(String i: score.keySet()) {
-				if(mark< score.get(i)) {
-					System.out.println("You got a "+i);
+			for(Integer i: score.keySet()) {
+				if(mark <= 39) {
+					System.out.println("You got a Fail");
+					System.exit(0);
+				}
+				if(mark>=i) {
+					System.out.println("You got a "+score.get(i));
 					break;
-				}		
-			}
+				}
+
+					
+				}
+				
+				
 		}
 			catch(InputMismatchException e) {
 				System.out.println("Invalid Input, program terminating");
